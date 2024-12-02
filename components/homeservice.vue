6a9e05c4566d851677bfc855938e3fba275c0e25
Lavanya077 <lavanyaprabhakaran77@gmail.com>
2024-12-02 09:08:27 +0530

    carosel updated

--- components/homeservice.vue
+++ components/homeservice.vue
@@ -1,115 +1,95 @@
 <template>
-    <!-- <div class="container mx-auto mt-10">
-      <div class="flex flex-col md:flex-row items-center">
-      
-        <div class="md:w-1/2 flex items-center justify-center p-5">
-          <div class="text-center md:text-left">
-            <h2 class="text-3xl font-bold mb-4">{{ activeContent.title }}</h2>
-            <p class="text-gray-600">{{ activeContent.description }}</p>
+  <div class="container mx-auto my-10">
+    <div class="flex flex-col md:flex-row items-center gap-5 justify-around px-20">
+  
+      <div class="md:w-1/3 relative">
+        <div class="relative overflow-hidden">
+          <div
+            v-for="(card, index) in cards"
+            :key="index"
+            v-show="index === activeIndex"
+            class="transition-opacity duration-500 flex justify-center"
+          >
+            <img
+              :src="card.image ? '/' + card.image : '/3d.jpg'"
+              alt="Carousel Image"
+              class="w-[350px] h-64 rounded-lg mt-6"
+            />
           </div>
         </div>
-  
-      
-        <div class="md:w-1/2 relative">
-         
-          <div class="relative overflow-hidden">
-            <div
-              v-for="(card, index) in cards"
-              :key="index"
-              class="absolute inset-0 transition-transform duration-700"
-              :class="{ 'translate-x-0': index === activeIndex, 'translate-x-full': index > activeIndex, '-translate-x-full': index < activeIndex }"
-            >
-              <img
-                :src="card.image"
-                alt="Carousel Image"
-                class="w-full h-60 object-cover rounded-lg"
-              />
-            </div>
-          </div>
-  
-          
-          <div class="flex justify-center mt-4 space-x-2">
-            <button
-              v-for="(card, index) in cards"
-              :key="index"
-              @click="setActiveIndex(index)"
-              class="w-3 h-3 rounded-full"
-              :class="index === activeIndex ? 'bg-blue-500' : 'bg-gray-400'"
-            ></button>
-          </div>
-  
-         
+
+    
+        <div class="flex justify-center items-center mt-4 space-x-2">
           <button
-            class="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white shadow-md rounded-full p-2 focus:outline-none"
-            @click="prevSlide"
-          >
-            <span class="material-icons text-gray-600">chevron_left</span>
-          </button>
-          <button
-            class="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white shadow-md rounded-full p-2 focus:outline-none"
-            @click="nextSlide"
-          >
-            <span class="material-icons text-gray-600">chevron_right</span>
-          </button>
+            v-for="(card, index) in cards"
+            :key="index"
+            @click="setActiveIndex(index)"
+            class="w-3 h-3 rounded-full mt-5"
+            :class="index === activeIndex ? 'bg-sky-900' : 'bg-gray-400'"
+          ></button>
         </div>
+
+       
+        <button
+          class="absolute left-24 transform -translate-y-1/2 shadow-md text-white rounded-full px-2 py-1 focus:outline-none z-10 bg-sky-900"
+          @click="prevSlide"
+        >
+          <span class="mdi mdi-arrow-left font-semibold"></span>
+        </button>
+        <button
+          class="absolute right-24 transform -translate-y-1/2 text-white bg-sky-900 shadow-md rounded-full px-2 py-1 focus:outline-none z-10"
+          @click="nextSlide"
+        >
+          <span class="mdi mdi-arrow-right"></span>
+        </button>
       </div>
-    </div> -->
-  </template>
-  
-  <!-- <script setup>
-  import { ref, computed } from 'vue';
-  
-  
-  const cards = ref([
-    {
-      image: "../assets/kitchen.vue",
-      title: "Card Title 1",
-      description: "This is a description for the first card."
-    },
-    {
-      image: "../assets/",
-      title: "Card Title 2",
-      description: "This is a description for the second card."
-    },
-    {
-      image: "",
-      title: "Card Title 3",
-      description: "This is a description for the third card."
-    },
-    {
-      image: "",
-      title: "Card Title 4",
-      description: "This is a description for the fourth card."
-    },
-    {
-      image: "",
-      title: "Card Title 5",
-      description: "This is a description for the fifth card."
-    }
-  ]);
-  const activeIndex = ref(0);
-  const activeContent = computed(() => cards.value[activeIndex.value]);
-  
- 
-  const nextSlide = () => {
-    activeIndex.value = (activeIndex.value + 1) % cards.value.length;
-  };
-  
-  const prevSlide = () => {
-    activeIndex.value =
-      (activeIndex.value - 1 + cards.value.length) % cards.value.length;
-  };
-  
-  
-  const setActiveIndex = (index) => {
-    activeIndex.value = index;
-  };
-  </script>
-  
-  <style scoped>
-  
-  button {
-    transition: background-color 0.3s ease;
-  }
-  </style>
-   -->
+
+    
+      <div class="md:w-2/3  ">
+        <div class="text-center md:text-left">
+          <h1 class="text-3xl font-bold text-sky-900 mb-4">
+            SERVICES WE ARE OFFERING
+          </h1>
+          <h2 class="text-2xl font-bold mb-4">{{ activeContent.title }}</h2>
+          <p class=" font-medium text-lg leading-7 text-gray-900 pb-4 ">
+            {{ activeContent.description }}
+          </p>
+        </div>
+        <router-link
+            :to="activeContent.route"
+            class="mt-3 inline-block bg-sky-900 text-white py-2 px-4 rounded-lg hover:bg-sky-700"
+          >
+            Learn More
+          </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { expertiseList } from '../data/service.json';
+const cards = ref(expertiseList);
+
+const activeIndex = ref(0);
+const activeContent = computed(() => cards.value[activeIndex.value]);
+
+
+const nextSlide = () => {
+  activeIndex.value = (activeIndex.value + 1) % cards.value.length;
+};
+
+const prevSlide = () => {
+  activeIndex.value = (activeIndex.value - 1 + cards.value.length) % cards.value.length;
+};
+
+const setActiveIndex = (index) => {
+  activeIndex.value = index;
+};
+</script>
+
+<style scoped>
+.transition-opacity {
+  transition: opacity 0.5s ease-in-out;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,19 @@ export default {
   content: [],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
       animation: {
         fade: 'fadeIn 0.5s ease-in-out',
       },
-    },
-  },
-  keyframes: {
-    fadeIn: {
-      '0%': { opacity: '0' },
-      '100%': { opacity: '1' },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
